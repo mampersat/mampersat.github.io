c2f7018bafe88bbfeb647005b78b0eaff44bd107
matt <msheppard@gmail.com>
2017-01-22 13:21:48 -0500

    multiple y axies

--- graph.py
+++ graph.py
@@ -7,16 +7,19 @@ start = datetime.now()
 conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="minion")
 
 query = """
-SELECT ts, t FROM report WHERE id = 1 and ts >= now() - INTERVAL 14 DAY;
+SELECT ts, t, lux FROM report WHERE id = 1 and ts >= now() - INTERVAL 14 DAY
 """
 
 dictCursor = conn.cursor(MySQLdb.cursors.Cursor)
 dictCursor.execute(query)
 data = dictCursor.fetchall()
 
-r = []
+temperature = []
+lux = []
 for row in data:
-    r.append([row[0], float(row[1]), ])
+    temperature.append([ int( row[0].strftime('%s')), float(row[1]), ])
+    if row[2]:
+        lux.append([ int( row[0].strftime('%s')), -float(row[2]), ])
 
 chart = Highchart()
 
@@ -25,14 +28,30 @@ options = {
                 'zoomType': 'x'
             },
             'title': {
-                'text': 'Minion 1 Temperature'
+                'text': 'Minion 1'
             },
-    }
+            'xAxis': {
+                'type': 'datetime'
+            },
+            'yAxis': [{
+                    'title': {
+                        'text':'temperature'
+                    },
+                },{
+                    'title': {
+                        'text': 'lux',
+                    },
+                    'opposite': 'true'
+                } ]
+        }
 
 chart.set_dict_options(options)
 
-chart.add_data_set(r, series_type='line', name='Example Series')
+chart.add_data_set(temperature, series_type='line', name='m1.temp', yAxis=0)
+chart.add_data_set(lux, series_type='line', name='m1.lux', yAxis=1)
+
 chart.save_file()
+
 
 duration = datetime.now() - start
 print duration.seconds, 's'
