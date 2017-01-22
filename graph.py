fdbe0e703f9c962e260c7dbc9db4e11c7fed35be
matt <msheppard@gmail.com>
2017-01-22 11:21:16 -0500

    14 day graph test

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ start = datetime.now()
 conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="minion")
 
 query = """
-SELECT ts, t FROM report WHERE id = 1 and ts >= now() - INTERVAL 1 DAY;
+SELECT ts, t FROM report WHERE id = 1 and ts >= now() - INTERVAL 14 DAY;
 """
 
 dictCursor = conn.cursor(MySQLdb.cursors.Cursor)
@@ -19,6 +19,18 @@ for row in data:
     r.append([row[0], float(row[1]), ])
 
 chart = Highchart()
+
+options = {
+            'chart': {
+                'zoomType': 'x'
+            },
+            'title': {
+                'text': 'Minion 1 Temperature'
+            },
+    }
+
+chart.set_dict_options(options)
+
 chart.add_data_set(r, series_type='line', name='Example Series')
 chart.save_file()
 
